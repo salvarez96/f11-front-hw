a4a382251382cc74b45a1f573fa2f2a25639173d
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-02-02 19:02:08 -0500

    Add token and session timeout

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,10 +1,15 @@
-import  apiFetch from "../../utils/apiFetch"
+import apiFetch from "../../utils/apiFetch"
 
 const intentLogin = async (payload) => {
 
     try {
         const {data} = await apiFetch().post('/usuarios/login', payload);
         localStorage.setItem('jwt_token', JSON.stringify(data.jwt))
+        const expirationTime = 20 * 60 * 1000 // min * 60 * 1000 para obtener tiempo en ms
+        setTimeout(() => {
+            localStorage.removeItem('jwt_token')
+            location.pathname = '/'
+        }, expirationTime)
         return data;
     } catch (error) {
         return Promise.reject(error)
